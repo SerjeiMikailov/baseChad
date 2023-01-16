435a9e9e3643afd08d21910760b209915b45bac1
SerjeiMikailov <felipemaders8@gmail.com>
2023-01-16 20:18:45 -0300

    function confirmcheckbox() {   alert("Você concordou com os termos chads"); }
    function darktheme() {
      const body = document.querySelector("body");
    
      if (body.classList.contains("boasvindas")) {
        body.classList.replace("boasvindas", "blackbody");
      } else {
        body.classList.replace("blackbody", "boasvindas");
      }
    }
    
    const withUniqueCall = (fn) => {
      let isCalled = false;
      return (...args) => {
        if (!isCalled) {
          isCalled = true;
          return fn(...args);
        }
      };
    };
    
    const uniqueLog = withUniqueCall(() => console.log("Called"));
    uniqueLog();
    uniqueLog();
    uniqueLog();
    uniqueLog();
    uniqueLog();
    uniqueLog();
    
    function exibirgaleriachad() {
        const elemento = document.getElementById ('galeriachad')
    
        elemento.innerHTML = elemento.innerHTML + '<img src="https://www.thesun.co.uk/wp-content/uploads/2021/10/OFF-PLAT-JD-GIGACHAD.jpg?w=660"></img>'
    }

--- base/ft.js
+++ base/ft.js
@@ -1,22 +1,37 @@
-let block = (true)
-
 function confirmcheckbox() {
-    alert ('Você concordou com os termos chads')
+  alert("Você concordou com os termos chads");
 }
+
+function darktheme() {
+  const body = document.querySelector("body");
+
+  if (body.classList.contains("boasvindas")) {
+    body.classList.replace("boasvindas", "blackbody");
+  } else {
+    body.classList.replace("blackbody", "boasvindas");
+  }
+}
+
+const withUniqueCall = (fn) => {
+  let isCalled = false;
+  return (...args) => {
+    if (!isCalled) {
+      isCalled = true;
+      return fn(...args);
+    }
+  };
+};
+
+const uniqueLog = withUniqueCall(() => console.log("Called"));
+uniqueLog();
+uniqueLog();
+uniqueLog();
+uniqueLog();
+uniqueLog();
+uniqueLog();
 
 function exibirgaleriachad() {
     const elemento = document.getElementById ('galeriachad')
 
-    elemento.innerHTML = elemento.innerHTML + '<img src="https://www.thesun.co.uk/wp-content/uploads/2021/10/OFF-PLAT-JD-GIGACHAD.jpg?w=660"></img>',
-    (false)
-} 
-
-function darktheme() {
-    const body = document.querySelector ('body')
-    
-    if (body.classList.contains('boasvindas')) {
-    body.classList.replace ('boasvindas', 'blackbody');
-} else {
-    body.classList.replace ('blackbody', 'boasvindas');
-}
+    elemento.innerHTML = elemento.innerHTML + '<img src="https://www.thesun.co.uk/wp-content/uploads/2021/10/OFF-PLAT-JD-GIGACHAD.jpg?w=660"></img>'
 }
