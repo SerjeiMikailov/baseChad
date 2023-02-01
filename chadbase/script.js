5fa0a1e221ed8399dde0c60020b291ccc584fbe0
SerjeiMikailov <felipemaders8@gmail.com>
2023-01-31 22:43:24 -0300

    clone

---
+++ chadbase/script.js
@@ -0,0 +1,44 @@
+
+function exibirgaleriachad() {
+    const elemento = document.getElementById ('galeriachad')
+
+    elemento.innerHTML = elemento.innerHTML + '<img src="https://www.thesun.co.uk/wp-content/uploads/2021/10/OFF-PLAT-JD-GIGACHAD.jpg?w=660"></img>',
+    (false)
+} 
+
+function darktheme() {
+    const body = document.querySelector ('body')
+    
+    if (body.classList.contains('boasvindas')) {
+    body.classList.replace ('boasvindas', 'blackbody');
+} else {
+    body.classList.replace ('blackbody', 'boasvindas');
+}
+}
+
+let el2 = document.getElementById('entrarnochad')
+
+let el1 = document.getElementById('naoapto')
+
+function showChadButton() {
+    if (el2.className = 'chadform1') {
+        el2.classList.remove('chadform1')
+    } 
+    el2.classList.add('chadform');
+
+    el1.classList.add('chadform1')
+
+    alert ('Você concordou com os termos chads')
+}
+
+function loginChad() {
+    if (el1.className = 'chadform') {
+        el1.classList.remove('chadform')
+    }
+    el1.classList.add('chadform1')
+
+    if (el2.className = 'chadform1') {
+        el2.classList.remove('chadform1')
+    } 
+    el2.classList.add('chadform')
+}
