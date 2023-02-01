e6bae36d039190e2b39f43c20a21d849b1569563
SerjeiMikailov <felipemaders8@gmail.com>
2023-02-01 16:08:16 -0300

    gg

--- chadbase/script.js
+++ chadbase/script.js
@@ -35,3 +35,21 @@ function showChadButton() {
 function gg() {
     alert('kk')
 }
+
+let qtdVezes = 0;
+
+function  usarFunc() {
+  if(qtdVezes >= 0){
+    const elemento = document.getElementById ('div-top-chads')
+
+    elemento.innerHTML = elemento.innerHTML + '<img class="topChads-class" src="https://www.thesun.co.uk/wp-content/uploads/2021/10/OFF-PLAT-JD-GIGACHAD.jpg?w=660"></img>'
+    elemento.innerHTML = elemento.innerHTML + '<img class="topChads-class" src="https://pbs.twimg.com/media/EURuzSWXkAAYKHW.jpg"></img>'
+    elemento.innerHTML = elemento.innerHTML + '<img class="topChads-class" src="https://img.ifunny.co/images/9edf405a8ea74ce8b27337812206d335e5f5206c6d40f4d8a357023966fc4c57_1.jpg"></img>'
+
+    return qtdVezes = -1;
+  } else {alert('Só consegues clicar uma vez')}
+  
+  qtdVezes++;
+  console.log('Você está usando a função pela ' + qtdVezes + ' vez');
+  
+}
